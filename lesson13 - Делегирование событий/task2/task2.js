270fc6cd46a05c5c4cd081df0e314882b648bcc2
Sergei Mikharevich <mikharevichsn@mail.ru>
2022-03-08 22:12:11 +0500

    fix

--- lesson13 - Делегирование событий/task2/task2.js
+++ lesson13 - Делегирование событий/task2/task2.js
@@ -3,24 +3,3 @@
 // при этом если есть уже зеленый квадрат,
 // то он становиться обратно красным и так можно кликать на любой квадрат,
 // он становиться зеленым, а старый зеленый квадрат обратно крассным и тд.
-
-const parent = document.querySelector('.parent');
-const blocks = document.querySelectorAll('.block');
-
-function removeClassActive() {
-  blocks.forEach((block) => {
-    block.classList.remove('active');
-  });
-}
-
-parent.addEventListener('click', (event) => {
-  const isBlock = event.target.classList.contains('block');
-  if (!isBlock) return;
-
-  if (event.target.classList.contains('active')) {
-    event.target.classList.remove('active');
-  } else {
-    removeClassActive();
-    event.target.classList.add('active');
-  }
-});
--- lesson13 - Делегирование событий/task3/task3.js
+++ lesson13 - Делегирование событий/task3/task3.js
@@ -6,25 +6,3 @@
 // Под таблицей должна быть кнопка «поменять цвета».
 // При ее нажатии все цвета клеточек меняются
 // на противоположные.
-
-const parent = document.querySelector('.parent');
-const btn = document.querySelector('.reverse');
-// const blocks = document.querySelectorAll('.block');
-
-// function reverseActiveClasses() {
-//   blocks.forEach((block) => {
-//     block.classList.toggle('active');
-//   });
-// }
-
-parent.addEventListener('click', (event) => {
-  if (!event.target.classList.contains('block')) return;
-
-  event.target.classList.toggle('active');
-});
-
-// btn.addEventListener('click', reverseActiveClasses);
-
-btn.addEventListener('click', () => {
-  parent.classList.toggle('reversed');
-});
